f539d1d15f9553ed1571504589edcdb3e5b233a6
linz <phoneticem@gmail.com>
2021-08-29 12:40:56 +0430

    material and strength password

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StrengthPasswordComponent } from './components/strength-password/strength-password.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import { SampleFormComponent } from './components/sample-form/sample-form.component';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StrengthPasswordComponent
+    StrengthPasswordComponent,
+    SampleFormComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        MatInputModule,
+        MatButtonModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/components/sample-form/sample-form.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import {AbstractControl, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {PasswordValidator} from '../../utils/validators/password-validator';
+import {ConfirmedPasswordValidator} from '../../utils/validators/confirmed-password-validator.validator';
+
+
+@Component({
+  selector: 'app-sample-form',
+  templateUrl: './sample-form.component.html',
+  styleUrls: ['./sample-form.component.sass']
+})
+export class SampleFormComponent implements OnInit {
+  public changePassword: FormGroup;
+  get f(): { [key: string]: AbstractControl } {
+    return this.changePassword.controls;
+  }
+  constructor(private formBuilder: FormBuilder) {
+    this.changePassword = this.formBuilder.group({
+      password: new FormControl('', Validators.required),
+      newPassword: new FormControl('', [
+        Validators.required,
+        Validators.compose([PasswordValidator.validPassword(true)]),
+      ]),
+      passwordMin: new FormControl({ value: false, disabled: true }), // strength password
+      passwordDigit: new FormControl({ value: false, disabled: true }), // strength password
+      passwordSpecial: new FormControl({ value: false, disabled: true }), // strength password
+      confirm_password: new FormControl('', [
+        Validators.required,
+      ]),
+
+    }, {
+      validators: ConfirmedPasswordValidator('newPassword', 'confirm_password')
+    });
+  }
+  ngOnInit(): void {
+  }
+  public changePasswordHandler() {
+    alert('Form submitted successfully');
+  }
+}
